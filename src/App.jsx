6e97b0f6e0f3d28d9f6e50e795ae8cc56f464519
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-12-16 23:04:52 +0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ export default function App() {
 
             <Charts />
 
-            <div>
+       
                 <TotalExpenses />
                 <TotalIncome />
-            </div>
+        
         </div>
     );
 }
